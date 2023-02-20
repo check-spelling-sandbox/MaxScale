690d90a537dfe93f1785f28671eb324de1d2ad89
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-02-20 17:54:27 -0500

    spelling: nosqlprotocol
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- server/modules/protocol/NoSQL/commands/user_management.hh
+++ server/modules/protocol/NoSQL/commands/user_management.hh
@@ -1505,7 +1505,7 @@ private:
 
                     ss << m_statements[i] << "\" failed with \"" << err.message() << "\". "
                        << "The grants in the MariaDB server and the roles in the local "
-                       << "nosqlprotocl database are now not in sync.";
+                       << "nosqlprotocol database are now not in sync.";
 
                     throw SoftError(ss.str(), error::INTERNAL_ERROR);
                 }
@@ -1553,7 +1553,7 @@ private:
                     ss << "but granting privileges with \"" << m_statements[i]
                        << "\" failed with \"" << err.message() << "\". "
                        << "The grants in the MariaDB server and the roles in the local "
-                       << "nosqlprotocl database are now not in sync.";
+                       << "nosqlprotocol database are now not in sync.";
 
                     throw SoftError(ss.str(), error::INTERNAL_ERROR);
                 }
